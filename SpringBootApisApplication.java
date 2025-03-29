45a2bda68d4474c5b9ec20c613a2a0538ce66e2a
sakshi98681 <sakshi98kadam@gmail.com>
2025-03-29 12:34:08 +0530

    Add files via upload

---
+++ SpringBootApisApplication.java
@@ -0,0 +1,14 @@
+package com.tka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApisApplication.class, args);
+		System.out.println("Application is Started.....");
+	}
+
+}
